611c9d035c3df3051915bbebac7645ea72139498
Gabesz79 <58592305+Gabesz79@users.noreply.github.com>
2026-02-09 14:02:32 +0100

    test: use test profile for contextLoads

--- src/test/java/hu/webuni/transport/TransportApplicationTests.java
+++ src/test/java/hu/webuni/transport/TransportApplicationTests.java
@@ -2,8 +2,10 @@ package hu.webuni.transport;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class TransportApplicationTests {
 
 	@Test
