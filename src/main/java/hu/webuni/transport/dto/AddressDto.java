0080ba9e51f3050271680eb49c0bacb021177a54
Gabesz79 <58592305+Gabesz79@users.noreply.github.com>
2026-02-10 09:06:08 +0100

    feat: add AddressDto and AddressMapper

--- src/main/java/hu/webuni/transport/dto/AddressDto.java
+++ src/main/java/hu/webuni/transport/dto/AddressDto.java
@@ -1,72 +1,51 @@
-package hu.webuni.transport.model;
+package hu.webuni.transport.dto;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+public class AddressDto {
 
-@Entity
-public class Address {
-	
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-
+	
 	private String country;
 	private String zip;
 	private String city;
 	private String street;
 	private String houseNumber;
-	
 	public Long getId() {
 		return id;
 	}
-
 	public void setId(Long id) {
 		this.id = id;
 	}
-
 	public String getCountry() {
 		return country;
 	}
-
 	public void setCountry(String country) {
 		this.country = country;
 	}
-
 	public String getZip() {
 		return zip;
 	}
-
 	public void setZip(String zip) {
 		this.zip = zip;
 	}
-
 	public String getCity() {
 		return city;
 	}
-
 	public void setCity(String city) {
 		this.city = city;
 	}
-
 	public String getStreet() {
 		return street;
 	}
-
 	public void setStreet(String street) {
 		this.street = street;
 	}
-
 	public String getHouseNumber() {
 		return houseNumber;
 	}
-
 	public void setHouseNumber(String houseNumber) {
 		this.houseNumber = houseNumber;
 	}
-
-	public Address(String country, String zip, String city, String street, String houseNumber) {
+	public AddressDto(String country, String zip, String city, String street, String houseNumber) {
 		super();
 		this.country = country;
 		this.zip = zip;
@@ -75,7 +54,8 @@ public class Address {
 		this.houseNumber = houseNumber;
 	}
 	
-	public Address() {
+	public AddressDto() {
 		
 	}
+	
 }
